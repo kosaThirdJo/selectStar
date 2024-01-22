3af8b02c61b925592e276c4dab216d995d14b2cd
syeej <syee2joe@gmail.com>
2024-01-22 15:42:39 +0900

    FIX: Token 수정

--- client.js/src/common.js
+++ client.js/src/common.js
@@ -24,6 +24,21 @@ const apiToken = async (urn, method, data, token) => {
         return { data: e}; //error 발생 시 e 반환
     })).data
 }
+const apiToken2 = async (urn, method, data, token) => {
+    const url = "http://"+  window.location.hostname + ":8081/" +  urn
+    // URL : URL 인대 URL URL이 같으면 생략 가능
+    return (await axios({
+        url,
+        method,
+        data,
+        headers: {
+            Authorization: token
+        }
+    }).catch(e => {
+        console.log(e);
+        return { data: e}; //error 발생 시 e 반환
+    }))
+}
 const api = async (urn, method, data) => {
     const url = "http://"+  window.location.hostname + ":8081/" +  urn
     // URL : URL 인대 URL URL이 같으면 생략 가능
@@ -36,7 +51,18 @@ const api = async (urn, method, data) => {
         return { data: e}; //error 발생 시 e 반환
     })).data
 }
-
+const api2 = async (urn, method, data) => {
+    const url = "http://"+  window.location.hostname + ":8081/" +  urn
+    // URL : URL 인대 URL URL이 같으면 생략 가능
+    return (await axios({
+        url,
+        method,
+        data
+    }).catch(e => {
+        console.log(e);
+        return { data: e}; //error 발생 시 e 반환
+    }))
+}
 const loginApi = async (urn, method, data) => {
     const url = "http://"+  window.location.hostname + ":8081/" +  urn
     return (await axios({
@@ -50,5 +76,5 @@ const loginApi = async (urn, method, data) => {
     }))
 }
 export {
-    api, apiToken, loginApi
+    api, apiToken, loginApi, api2, apiToken2
 };
