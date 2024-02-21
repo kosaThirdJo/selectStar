0f8fc2f16cf47b8649044c5d762a5d07b0fd4609
gossong <thdwldh99@gmail.com>
2024-02-21 14:08:50 +0900

    fix: common.js 수정

--- client.js/src/common.js
+++ client.js/src/common.js
@@ -52,10 +52,10 @@ const apiToken2 = async (urn, method, data, token) => {
             }
         });
         if (accessResponse.data.valid === true) {
-            console.log('Access 토큰 유효');
+            // console.log('Access 토큰 유효');
             return await sendRequestWithToken(url, method, data, token);
         } else {
-            console.log('Access 토큰이 만료되었거나 유효하지 않음');
+            // console.log('Access 토큰이 만료되었거나 유효하지 않음');
 
             // Access Token이 만료되면 Refresh Token을 통해 새로운 Access Token을 요청
             const refreshUrl = `http://${window.location.hostname}:8081/users/validate-refresh-token`;
@@ -66,15 +66,14 @@ const apiToken2 = async (urn, method, data, token) => {
             if (refreshResponse.data.valid) {
                 const newAccessToken = "Bearer " + refreshResponse.data.newAccessToken;
                 localStorage.setItem("jwtToken", newAccessToken); // 새로운 Access Token을 로컬 스토리지에 저장
-                console.log(newAccessToken);
                 return await sendRequestWithToken(url, method, data, newAccessToken); // 새로운 Access Token으로 요청 재시도
             } else {
-                console.log('Refresh Token이 만료되었거나 유효하지 않음');
+                // console.log('Refresh Token이 만료되었거나 유효하지 않음');
                 await handleLogout();
             }
         }
     } catch (error) {
-        console.error('토큰 유효성 확인 실패', error);
+        // console.error('토큰 유효성 확인 실패', error);
         if (error.response && error.response.status === 401) {
             // 4. 로그아웃 처리
             await handleLogout();
